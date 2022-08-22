daa171f583dd9fe7ee639dc90058aa8ec5aa85d7
Filip Karlsson <xfac115@gmail.com>
2022-08-22 13:46:07 +0200

    Camera and projectileontime
    -Camera is zoomed out more
    -ProjectileOnTime spawns new projectile with new direction. Direction rotated with n and angle. Shoots minimum two bullets after the time and uses the direction of parent bullet and rotate it with an angle both negative and positive ( <- ->).
    -Moved bullet creation to its own function InitBullet to seperate.

--- Scripts/ProjectileOnTime.gd
+++ Scripts/ProjectileOnTime.gd
@@ -4,15 +4,25 @@ func LifetimeEnd():
 	call_deferred("SpawnProjectiles",2)
 	queue_free()
 func SpawnProjectiles(quantity):
-	var tempDirection:Vector2 = direction.normalized()
-	var angle = 30
-	for i in quantity:
-		var newBullet = projectileToSpawn.instance()
-		theOwner.add_child(newBullet)
-		newBullet.position = position
-		newBullet.damage = damage
-		newBullet.speed = speed*5
-		tempDirection = tempDirection.rotated(deg2rad(angle))
-		newBullet.SetDirection(tempDirection)
-		newBullet.theOwner = theOwner
-		angle = (angle*-1)+30
+	var initDirection:Vector2 = direction.normalized()
+	var angle = 20
+	var n = 1
+	
+	for i in n:
+		var x = 1
+		for j in 2:
+			var newBullet = InitBullet()
+			var tempDirection = initDirection
+			tempDirection = tempDirection.rotated(deg2rad(angle * (i+1) * x))
+			newBullet.SetDirection(tempDirection)
+			x = x * -1
+
+
+func InitBullet():
+	var newBullet = projectileToSpawn.instance()
+	theOwner.add_child(newBullet)
+	newBullet.position = position
+	newBullet.damage = damage
+	newBullet.speed = speed*5
+	newBullet.theOwner = theOwner
+	return newBullet
