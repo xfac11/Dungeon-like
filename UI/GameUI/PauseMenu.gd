fbd13f4d7533e0b32c598a4752a23757e8ca248d
Filip Karlsson <xfac115@gmail.com>
2023-07-24 09:08:28 +0200

    OptionMenu in PauseMenu
    -OptionMenu now displays when "Option" button in PauseMenu is pressed and works correctly as in the MainMenu.
    -Reset save button is removed so it is only possible in the MainMenu to reset a save. Not during gameplay

--- UI/GameUI/PauseMenu.gd
+++ UI/GameUI/PauseMenu.gd
@@ -1,6 +1,8 @@
 extends Panel
+onready var optionMenu = $OptionMenu
 onready var statsLabel = $StatsPanel/VBoxContainer/StatsLabel
 signal Pause(isPaused)
+
 func onResumeClicked():
 	visible = false
 	emit_signal("Pause",false)
@@ -8,3 +10,7 @@ func onOptionsClicked():
 	pass
 func onQuitClicked():
 	pass
+
+
+func _on_OptionsButton_pressed():
+	optionMenu.visible = true
